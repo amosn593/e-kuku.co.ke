3a116957876dce3b7734181bc22e0f6a4da724f8
Amos Ndonga <amosndonga@gmail.com>
2021-07-27 10:50:45 +0300

    added font awesome icons

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,16 +5,19 @@ function Home() {
     <div className="row m-2">
         <div className="col-md-3">
             <div className="card text-center shadow">
-                <div className="card-header">
-                    Featured
+                <div className="card-header text-muted d-flex justify-content-between">
+                    <i class="fa fa-map-marker" aria-hidden="true">KITUI</i>
+                    <i class="fa fa-map-marker" aria-hidden="true">Ikutha</i>
                 </div>
                 <div className="card-body">
-                    <h5 className="card-title">Special title treatment</h5>
-                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
+                    <h6 className="card-title">Kienyeji Eggs</h6>
+                    <p className="card-text">Price: Negotiable</p>
+                    <div>
+                        <button className="btn btn-dark">View</button>    
+                    </div>
                 </div>
                 <div className="card-footer text-muted">
-                    2 days ago
+                    1 May, 2021
                 </div>
             </div>
         </div>
